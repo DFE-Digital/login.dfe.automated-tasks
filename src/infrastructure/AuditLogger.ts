ab3bb45453d538637686906060faeb6743f163fe
Jared Marsh <jared.marsh@education.gov.uk>
2024-12-12 19:51:27 +0000

    Add batched logging to service bus for auditing
    Azure allows up to 100 messages before batch or if it hits the size limitation.

--- src/infrastructure/AuditLogger.ts
+++ src/infrastructure/AuditLogger.ts
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { ServiceBusClient } from "@azure/service-bus";
+import { ServiceBusClient, ServiceBusMessage } from "@azure/service-bus";
 import { WebSocket } from "ws";
 
 /**
@@ -42,29 +42,67 @@ export class AuditLogger {
   };
 
   /**
+   * Adds default values for optional audit log fields and turn into the service bus format.
+   *
+   * @param record - An audit log record ({@link AuditLog}).
+   * @returns A formatted audit log message ({@link ServiceBusMessage}).
+   */
+  private formatLog(record: AuditLog): ServiceBusMessage {
+    const formattedRecord = { ...record };
+
+    if (!formattedRecord.level) {
+      formattedRecord.level = "audit";
+    }
+
+    if (!formattedRecord.application) {
+      formattedRecord.application = "automated-tasks";
+    }
+
+    if (!formattedRecord.env) {
+      formattedRecord.env = "azure";
+    }
+
+    return {
+      body: JSON.stringify([JSON.stringify(formattedRecord)]),
+    };
+  };
+
+  /**
+   * Logs multiple records in batches to the service bus so it can go into the DSi audit tables.
+   *
+   * @param records - An array of audit log records ({@link AuditLog}) to be batch sent to the database.
+   *
+   * @throws Error if the service bus connection fails, or the message batch fails to send.
+   */
+  async batchedLog(records: AuditLog[]): Promise<void> {
+    const batchSize = 100; // Azure service bus can send a max of 100 messages per batch.
+    const sender = this.serviceBus.createSender(process.env.AUDIT_TOPIC_NAME);
+
+    for (let index = 0; index < records.length; index += batchSize) {
+      const batch = records.slice(index, index + batchSize);
+
+      try {
+        await sender.sendMessages(batch.map(record => this.formatLog(record)));
+      } catch (error) {
+        throw new Error(`Audit service bus message failed to send with error "${error.message}"`);
+      }
+    }
+
+    await sender.close();
+  };
+
+  /**
    * Logs a record to the service bus so it can go into the DSi audit tables.
    *
-   * @param record - An audit log {@link AuditLog} record to be put into the database.
+   * @param record - An audit log record ({@link AuditLog}) to be put into the database.
+   *
+   * @throws Error if the service bus connection fails, or the message fails to send.
    */
   async log(record: AuditLog): Promise<void> {
     const sender = this.serviceBus.createSender(process.env.AUDIT_TOPIC_NAME);
 
-    if (!record.level) {
-      record.level = "audit";
-    }
-
-    if (!record.application) {
-      record.application = "automated-tasks";
-    }
-
-    if (!record.env) {
-      record.env = "azure";
-    }
-
     try {
-      await sender.sendMessages({
-        body: JSON.stringify([JSON.stringify(record)]),
-      });
+      await sender.sendMessages(this.formatLog(record));
     } catch (error) {
       throw new Error(`Audit service bus message failed to send with error "${error.message}"`);
     }
--- test/infrastructure/AuditLogger.test.ts
+++ test/infrastructure/AuditLogger.test.ts
@@ -63,7 +63,7 @@ describe("Service bus audit logger", () => {
     });
   });
 
-  describe("log", () => {
+  describe("Logging functions", () => {
     const sendMessages = jest.fn();
     const close = jest.fn();
     const createSender = jest.fn();
@@ -83,73 +83,185 @@ describe("Service bus audit logger", () => {
       auditLogger = new AuditLogger();
     });
 
-    it("it creates a sender using the topic name environment variable", async () => {
-      const topicName = "testingTopic";
-      process.env.AUDIT_TOPIC_NAME = topicName;
-      await auditLogger.log(minimumLog);
+    describe("batchedLog", () => {
+      it("it creates a sender using the topic name environment variable", async () => {
+        const topicName = "testingTopic";
+        process.env.AUDIT_TOPIC_NAME = topicName;
+        await auditLogger.batchedLog([minimumLog]);
 
-      expect(createSender).toHaveBeenCalled();
-      expect(createSender).toHaveBeenCalledWith(topicName);
-    });
+        expect(createSender).toHaveBeenCalled();
+        expect(createSender).toHaveBeenCalledWith(topicName);
+      });
 
-    it("it sets the audit record's level to 'audit' if it is not set", async () => {
-      await auditLogger.log(minimumLog);
+      it("it sets each audit record's level to 'audit' if it is not set", async () => {
+        const level = "testLevel";
+        const logs = [
+          minimumLog,
+          minimumLog, {
+          ...minimumLog,
+          level,
+        }]; 
+        await auditLogger.batchedLog(logs);
 
-      expect(sendMessages).toHaveBeenCalled();
-      expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0].body)[0])).toHaveProperty("level", "audit");
-    });
+        expect(sendMessages).toHaveBeenCalled();
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][0].body)[0])).toHaveProperty("level", "audit");
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][1].body)[0])).toHaveProperty("level", "audit");
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][2].body)[0])).toHaveProperty("level", level);
+      });
 
-    it("it sets the audit record's application to 'automated-tasks' if it is not set", async () => {
-      await auditLogger.log(minimumLog);
+      it("it sets each audit record's application to 'automated-tasks' if it is not set", async () => {
+        const application = "testApplication";
+        const logs = [
+          minimumLog,
+          minimumLog, {
+          ...minimumLog,
+          application,
+        }]; 
+        await auditLogger.batchedLog(logs);
 
-      expect(sendMessages).toHaveBeenCalled();
-      expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0].body)[0])).toHaveProperty("application", "automated-tasks");
-    });
+        expect(sendMessages).toHaveBeenCalled();
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][0].body)[0])).toHaveProperty("application", "automated-tasks");
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][1].body)[0])).toHaveProperty("application", "automated-tasks");
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][2].body)[0])).toHaveProperty("application", application);
+      });
 
-    it("it sets the audit record's env to 'azure' if it is not set", async () => {
-      await auditLogger.log(minimumLog);
+      it("it sets each audit record's env to 'azure' if it is not set", async () => {
+        const env = "testing";
+        const logs = [
+          minimumLog,
+          minimumLog, {
+          ...minimumLog,
+          env,
+        }]; 
+        await auditLogger.batchedLog(logs);
 
-      expect(sendMessages).toHaveBeenCalled();
-      expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0].body)[0])).toHaveProperty("env", "azure");
-    });
+        expect(sendMessages).toHaveBeenCalled();
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][0].body)[0])).toHaveProperty("env", "azure");
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][1].body)[0])).toHaveProperty("env", "azure");
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0][2].body)[0])).toHaveProperty("env", env);
+      });
 
-    it("it sends the JSON stringified array of the stringified body to the service bus topic", async () => {
-      const fullLog = {
-        level: "test",
-        message: "test",
-        application: "test",
-        env: "test",
-        type: "test",
-        subType: "test",
-        userId: "test",
-        organisationid: "test",
-        meta: {
-          test: "test"
-        },
-      };
-      await auditLogger.log(fullLog);
+      it("it sends the JSON stringified array of the stringified body for each message to the service bus topic", async () => {
+        const fullLog = {
+          level: "test",
+          message: "test",
+          application: "test",
+          env: "test",
+          type: "test",
+          subType: "test",
+          userId: "test",
+          organisationid: "test",
+          meta: {
+            test: "test"
+          },
+        };
+        const message = {
+          body: JSON.stringify([JSON.stringify(fullLog)]),
+        };
+        await auditLogger.batchedLog([fullLog, fullLog, fullLog]);
 
-      expect(sendMessages).toHaveBeenCalled();
-      expect(sendMessages).toHaveBeenCalledWith({
-        body: JSON.stringify([JSON.stringify(fullLog)]),
+        expect(sendMessages).toHaveBeenCalled();
+        expect(sendMessages).toHaveBeenCalledWith([message, message, message]);
+      });
+
+      it("it sends multiple batches of messages if the number of messages > largest batch size (100)", async () => {
+        const messages = Array(305).fill(minimumLog);
+        await auditLogger.batchedLog(messages);
+
+        expect(sendMessages).toHaveBeenCalledTimes(4);
+        expect(sendMessages.mock.calls[0][0].length).toEqual(100);
+        expect(sendMessages.mock.calls[1][0].length).toEqual(100);
+        expect(sendMessages.mock.calls[2][0].length).toEqual(100);
+        expect(sendMessages.mock.calls[3][0].length).toEqual(5);
+      });
+
+      it("it throws an error if the service bus message fails to send", async () => {
+        const errorMessage = "Test error message";
+        sendMessages.mockImplementation(() => {
+          throw new Error(errorMessage);
+        });
+
+        expect(auditLogger.batchedLog([minimumLog])).rejects.toThrow(
+          `Audit service bus message failed to send with error "${errorMessage}"`
+        );
+      });
+
+      it("it closes the sender after it has been used", async () => {
+        await auditLogger.batchedLog([minimumLog]);
+
+        expect(close).toHaveBeenCalled();
       });
     });
 
-    it("it throws an error if the service bus message fails to send", async () => {
-      const errorMessage = "Test error message";
-      sendMessages.mockImplementation(() => {
-        throw new Error(errorMessage);
+    describe("log", () => {
+      it("it creates a sender using the topic name environment variable", async () => {
+        const topicName = "testingTopic";
+        process.env.AUDIT_TOPIC_NAME = topicName;
+        await auditLogger.log(minimumLog);
+
+        expect(createSender).toHaveBeenCalled();
+        expect(createSender).toHaveBeenCalledWith(topicName);
       });
 
-      expect(auditLogger.log(minimumLog)).rejects.toThrow(
-        `Audit service bus message failed to send with error "${errorMessage}"`
-      );
-    });
+      it("it sets the audit record's level to 'audit' if it is not set", async () => {
+        await auditLogger.log(minimumLog);
 
-    it("it closes the sender after it has been used", async () => {
-      await auditLogger.log(minimumLog);
+        expect(sendMessages).toHaveBeenCalled();
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0].body)[0])).toHaveProperty("level", "audit");
+      });
 
-      expect(close).toHaveBeenCalled();
+      it("it sets the audit record's application to 'automated-tasks' if it is not set", async () => {
+        await auditLogger.log(minimumLog);
+
+        expect(sendMessages).toHaveBeenCalled();
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0].body)[0])).toHaveProperty("application", "automated-tasks");
+      });
+
+      it("it sets the audit record's env to 'azure' if it is not set", async () => {
+        await auditLogger.log(minimumLog);
+
+        expect(sendMessages).toHaveBeenCalled();
+        expect(JSON.parse(JSON.parse(sendMessages.mock.calls[0][0].body)[0])).toHaveProperty("env", "azure");
+      });
+
+      it("it sends the JSON stringified array of the stringified body to the service bus topic", async () => {
+        const fullLog = {
+          level: "test",
+          message: "test",
+          application: "test",
+          env: "test",
+          type: "test",
+          subType: "test",
+          userId: "test",
+          organisationid: "test",
+          meta: {
+            test: "test"
+          },
+        };
+        await auditLogger.log(fullLog);
+
+        expect(sendMessages).toHaveBeenCalled();
+        expect(sendMessages).toHaveBeenCalledWith({
+          body: JSON.stringify([JSON.stringify(fullLog)]),
+        });
+      });
+
+      it("it throws an error if the service bus message fails to send", async () => {
+        const errorMessage = "Test error message";
+        sendMessages.mockImplementation(() => {
+          throw new Error(errorMessage);
+        });
+
+        expect(auditLogger.log(minimumLog)).rejects.toThrow(
+          `Audit service bus message failed to send with error "${errorMessage}"`
+        );
+      });
+
+      it("it closes the sender after it has been used", async () => {
+        await auditLogger.log(minimumLog);
+
+        expect(close).toHaveBeenCalled();
+      });
     });
   });
 });
