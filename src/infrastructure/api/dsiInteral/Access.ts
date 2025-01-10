71a71854ec94ed7ff037086187c0bb5a7045d030
Jared Marsh <jared.marsh@education.gov.uk>
2025-01-10 10:05:36 +0000

    Add methods to delete user/invitation services using the Access API

--- src/infrastructure/api/dsiInteral/Access.ts
+++ src/infrastructure/api/dsiInteral/Access.ts
@@ -1,3 +1,4 @@
+import { ApiRequestMethod } from "../common/ApiClient";
 import { ApiName, DsiInternalApiClient } from "./DsiInternalApiClient"
 
 /**
@@ -13,5 +14,45 @@ export class Access {
    */
   constructor() {
     this.client = new DsiInternalApiClient(ApiName.Access);
+  };
+
+  /**
+   * Deletes a service link from an invitation in an organisation.
+   *
+   * @param invitationId - The ID of the invitation to remove the service from.
+   * @param serviceId - The ID of the service to be removed from the invitation.
+   * @param organisationId - The ID of the organisation the invitation has the service in.
+   * @param correlationId - Correlation ID to be passed with the request.
+   * @returns true if the invitation's service link was successfully deleted, false otherwise.
+   *
+   * @throws Error when the API client throws.
+   */
+  async deleteInvitationService(invitationId: string, serviceId: string, organisationId: string, correlationId: string): Promise<boolean> {
+    const response = await this.client.requestRaw(
+      ApiRequestMethod.DELETE,
+      `/invitations/${invitationId}/services/${serviceId}/organisations/${organisationId}`, {
+      correlationId,
+    });
+    return response.status === 204;
+  };
+
+  /**
+   * Deletes a service link from a user in an organisation.
+   *
+   * @param userId - The ID of the user to remove the service from.
+   * @param serviceId - The ID of the service to be removed from the user.
+   * @param organisationId - The ID of the organisation the user has the service in.
+   * @param correlationId - Correlation ID to be passed with the request.
+   * @returns true if the user's service link was successfully deleted, false otherwise.
+   *
+   * @throws Error when the API client throws.
+   */
+  async deleteUserService(userId: string, serviceId: string, organisationId: string, correlationId: string): Promise<boolean> {
+    const response = await this.client.requestRaw(
+      ApiRequestMethod.DELETE,
+      `/users/${userId}/services/${serviceId}/organisations/${organisationId}`, {
+      correlationId,
+    });
+    return response.status === 204;
   };
 };
--- test/infrastructure/api/dsiInternal/Access.test.ts
+++ test/infrastructure/api/dsiInternal/Access.test.ts
@@ -1,5 +1,6 @@
 import { ApiName, DsiInternalApiClient } from "../../../../src/infrastructure/api/dsiInteral/DsiInternalApiClient";
 import { Access } from "../../../../src/infrastructure/api/dsiInteral/Access";
+import { ApiRequestMethod } from "../../../../src/infrastructure/api/common/ApiClient";
 
 jest.mock("../../../../src/infrastructure/api/dsiInteral/DsiInternalApiClient");
 
@@ -25,9 +26,9 @@ describe("Access API wrapper", () => {
   });
 
   describe("Access API functions", () => {
-    const setRequestRawResponse = (text?: string | null) => {
+    const setRequestRawResponse = (status: Number = 200) => {
       internalClient.prototype.requestRaw.mockResolvedValue({
-        text: () => Promise.resolve(text),
+        status,
       } as Response);
     };
     const setRequestResponse = (object: Object) => {
@@ -35,10 +36,124 @@ describe("Access API wrapper", () => {
     };
     let access: Access;
 
-    // beforeEach(() => {
-    //   setRequestRawResponse("");
-    //   setRequestResponse({});
-    //   access = new Access();
-    // });
+    beforeEach(() => {
+      setRequestRawResponse();
+      setRequestResponse({});
+      access = new Access();
+    });
+
+    describe("deleteInvitationService", () => {
+      it("it calls requestRaw using the DELETE method", async () => {
+        await access.deleteInvitationService("inv-1", "svc-1", "org-1", "correlation");
+
+        expect(internalClient.prototype.requestRaw).toHaveBeenCalled();
+        expect(internalClient.prototype.requestRaw.mock.calls[0][0]).toEqual(ApiRequestMethod.DELETE);
+      });
+
+      it("it calls requestRaw to the correct path with the passed invitation, service & org IDs", async () => {
+        const invId = "test-123";
+        const svcId = "service-123";
+        const orgId = "org-123";
+        await access.deleteInvitationService(invId, svcId, orgId, "");
+
+        expect(internalClient.prototype.requestRaw).toHaveBeenCalled();
+        expect(internalClient.prototype.requestRaw.mock.calls[0][1]).toEqual(
+          `/invitations/${invId}/services/${svcId}/organisations/${orgId}`
+        );
+      });
+
+      it("it calls requestRaw with the passed correlation ID", async () => {
+        const correlationId = "test-123";
+        await access.deleteInvitationService("", "", "", correlationId);
+
+        expect(internalClient.prototype.requestRaw).toHaveBeenCalled();
+        expect(internalClient.prototype.requestRaw.mock.calls[0][2]).toEqual({
+          correlationId,
+        });
+      });
+
+      it("it returns true if the response status is 204", async () => {
+        setRequestRawResponse(204);
+
+        expect(await access.deleteInvitationService("", "", "", "")).toEqual(true);
+      });
+
+      it.each([
+        201,
+        202,
+        302,
+        304,
+      ])("it returns false if the response status is not 204 (%p)", async (status) => {
+        setRequestRawResponse(status);
+
+        expect(await access.deleteInvitationService("", "", "", "")).toEqual(false);
+      });
+
+      it("it re-throws any errors thrown by requestRaw", async () => {
+        const errorMessage = "This is a test error";
+        internalClient.prototype.requestRaw.mockImplementation(() => {
+          throw new Error(errorMessage);
+        });
+
+        expect(access.deleteInvitationService("", "", "", "")).rejects.toThrow(errorMessage);
+      });
+    });
+
+    describe("deleteUserService", () => {
+      it("it calls requestRaw using the DELETE method", async () => {
+        await access.deleteUserService("user-1", "svc-1", "org-1", "correlation");
+
+        expect(internalClient.prototype.requestRaw).toHaveBeenCalled();
+        expect(internalClient.prototype.requestRaw.mock.calls[0][0]).toEqual(ApiRequestMethod.DELETE);
+      });
+
+      it("it calls requestRaw to the correct path with the passed user, service & org IDs", async () => {
+        const userId = "test-123";
+        const svcId = "service-123";
+        const orgId = "org-123";
+        await access.deleteUserService(userId, svcId, orgId, "");
+
+        expect(internalClient.prototype.requestRaw).toHaveBeenCalled();
+        expect(internalClient.prototype.requestRaw.mock.calls[0][1]).toEqual(
+          `/users/${userId}/services/${svcId}/organisations/${orgId}`
+        );
+      });
+
+      it("it calls requestRaw with the passed correlation ID", async () => {
+        const correlationId = "test-123";
+        await access.deleteUserService("", "", "", correlationId);
+
+        expect(internalClient.prototype.requestRaw).toHaveBeenCalled();
+        expect(internalClient.prototype.requestRaw.mock.calls[0][2]).toEqual({
+          correlationId,
+        });
+      });
+
+      it("it returns true if the response status is 204", async () => {
+        setRequestRawResponse(204);
+
+        expect(await access.deleteUserService("", "", "", "")).toEqual(true);
+      });
+
+      it.each([
+        201,
+        202,
+        302,
+        304,
+      ])("it returns false if the response status is not 204 (%p)", async (status) => {
+        setRequestRawResponse(status);
+
+        expect(await access.deleteUserService("", "", "", "")).toEqual(false);
+      });
+
+      it("it re-throws any errors thrown by requestRaw", async () => {
+        const errorMessage = "This is a test error";
+        internalClient.prototype.requestRaw.mockImplementation(() => {
+          throw new Error(errorMessage);
+        });
+
+        expect(access.deleteUserService("", "", "", "")).rejects.toThrow(errorMessage);
+      });
+    });
   });
 });
