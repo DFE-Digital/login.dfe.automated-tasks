085ebbdf4608e6759f7983b61ba3769379273201
Jared Marsh <jared.marsh@education.gov.uk>
2025-03-04 12:43:50 +0000

    Add invitation callback model and invitation initialisation utility function
    Callback model can be used separately too, and all invitation models can be initialised at once.

--- src/infrastructure/database/common/utils.ts
+++ src/infrastructure/database/common/utils.ts
@@ -1,6 +1,8 @@
-import { type Sequelize } from "sequelize";
+import { DataTypes, type Sequelize } from "sequelize";
 import { initialiseUser, User } from "../directories/User";
 import { initialiseUserPasswordPolicy, UserPasswordPolicy } from "../directories/UserPasswordPolicy";
+import { initialiseInvitation, Invitation } from "../directories/Invitation";
+import { initialiseInvitationCallback, InvitationCallback } from "../directories/InvitationCallback";
 import { initialiseUserBanner } from "../organisations/UserBanner";
 import { initialiseUserOrganisationRequest } from "../organisations/UserOrganisationRequest";
 import { initialiseUserServiceRequest } from "../organisations/UserServiceRequest";
@@ -30,5 +32,27 @@ export function initialiseAllUserModels(directoriesConnection: Sequelize, organi
   UserPasswordPolicy.belongsTo(User, {
     foreignKey: userPasswordPolicyFk,
     as: "user",
+  });
+};
+
+/**
+ * Initialise all "Invitation..." models including Invitation, and create model relationships.
+ *
+ * @param directoriesConnection - A {@link Sequelize} object connected to the directories database.
+ */
+export function initialiseAllInvitationModels(directoriesConnection: Sequelize): void {
+  initialiseInvitation(directoriesConnection);
+  initialiseInvitationCallback(directoriesConnection);
+  
+  Invitation.hasMany(InvitationCallback, {
+    foreignKey: {
+      name: "invitationId",
+      allowNull: false,
+    },
+    keyType: DataTypes.UUID,
+    as: "callbacks",
+  });
+  InvitationCallback.belongsTo(Invitation, {
+    as: "invitation",
   });
 };
---
+++ test/infrastructure/database/directories/InvitationCallback.test.ts
@@ -0,0 +1,47 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+import { initialiseInvitationCallback } from "../../../../src/infrastructure/database/directories/InvitationCallback";
+
+jest.mock("sequelize");
+
+describe("InvitationCallback database model", () => {
+  const model = jest.mocked(Model);
+
+  describe("initialiseInvitationCallback", () => {
+    it("it initialises the invitation callback model with the expected attributes and passed sequelize connection", () => {
+      const connection = new Sequelize();
+      initialiseInvitationCallback(connection);
+
+      expect(model.init).toHaveBeenCalled();
+      expect(model.init).toHaveBeenCalledWith({
+        invitationId: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          allowNull: false,
+        },
+        sourceId: {
+          type: DataTypes.STRING(255),
+          primaryKey: true,
+          allowNull: false,
+        },
+        callbackUrl: {
+          type: DataTypes.STRING(1024),
+          allowNull: false,
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        clientId: {
+          type: DataTypes.STRING(50),
+        },
+      }, {
+        tableName: "invitation_callback",
+        sequelize: connection,
+      });
+    });
+  });
+});
