7497d36572056564a70ab8ca726b4021f00b4acc
Univer-lion-lab <98389729+Univer-lion-lab@users.noreply.github.com>
2025-04-29 09:58:11 +0100

    [DSI-7621] Docker migration (#8)
    * Provide deactivation reason
    
    * Docker Migration & ESLint updated
    
    * Update package-lock to support ESLint
    
    Additionally fixes a vulnerability.
    
    * Update ESLint config to include test directory for Jest linting
    
    * Manual ESLint fixes
    
    * Add lint and test report files to .gitignore
    
    * Update azure-pipeline.yml
    
    ---------
    
    Co-authored-by: Adam Mann <Adam.Mann@education.gov.uk>
    Co-authored-by: Jared Marsh <jared.marsh@education.gov.uk>

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+const globals = require("globals");
+const pluginJest = require("eslint-plugin-jest");
+const pluginJs = require("@eslint/js");
+const pluginTs = require("typescript-eslint");
+
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
+module.exports = [
+  {
+    plugins: {
+      jest: pluginJest,
+    },
+  },
+
+  pluginJs.configs.recommended,
+  ...pluginTs.configs.recommended,
+
+  {
+    ignores: [
+      "coverage/**",
+      "build/**",
+      "dist/**",
+      "eslint.config.js",
+      "jest.config.js",
+    ],
+  },
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          impliedStrict: true,
+        },
+      },
+    },
+    rules: {
+      strict: ["error", "never"],
+    },
+  },
+
+  {
+    files: ["test/**/*.test.ts", "src/**/*.test.ts"],
+    languageOptions: {
+      globals: {
+        ...pluginJest.environments.globals.globals,
+      },
+    },
+    rules: {
+      // The following linting is disabled for unit tests to simplify mocking.
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+];
